b79cc484d78e49b383218f3cdf7ebb1fb3c18b9c
Ashita <ashita9820@gmail.com>
2021-02-25 13:17:59 +0400

    added all files

---
+++ products.js
@@ -0,0 +1,30 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bag of food",
+        "price": 20.00,
+        "image": "cat-food.jpg",
+        "availableInventory": 20,
+        "rating": 3
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with",
+        "price": 10.00,
+        "image": "yarn.jpg", 
+        "availableInventory": 20,
+        "rating": 2
+    },
+    {
+        "id": 1003,
+        "title": "Kitty Litter",
+        "description": "Premium kitty litter for your cat",
+        "price": 65.00,
+        "image": "litter.jpg",
+        "availableInventory": 10,
+        "rating": 4
+    }
+
+]
